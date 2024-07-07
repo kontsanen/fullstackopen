56c0cb3b37908aeac981e7ae797578fac9b05134
raitank4 <konsta.raitanen@aalto.fi>
2024-07-07 19:28:23 +0300

    done tasks 1.6. - 1.10

---
+++ osa1/unicafe/part1/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+const Button = ({handleClick, text}) => {
+  
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <div>
+      <p>
+        {props.text} {props.value}
+      </p>
+    </div>
+  )
+}
+
+
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad
+  const avg = (props.bad * (-1) + props.good) / (all)
+  const pos = props.good / (all) * 100
+
+  return (
+    <div>
+      {all > 0 ? (
+        <div>
+        <h1>statistics</h1>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={avg} />
+          <StatisticLine text="positive" value={pos} />
+        </div>
+       ) : (
+        <p>No feedback given</p>
+       )
+      }
+    </div>
+  )
+}
+
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text={"good"} />
+      <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"} />
+      <Button handleClick={() => setBad(bad + 1)} text={"bad"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
