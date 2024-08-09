570a605e01aa4154c6f47abc023939cf93f1bc5e
raitank4 <konsta.raitanen@aalto.fi>
2024-08-09 17:20:46 +0300

    done until 2.10

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,13 +1,65 @@
 import { useState } from 'react'
 
+const Filter = (props) => {
+  return (
+    <div>
+          Filter shown with 
+          <input
+            value={props.searchValue}
+            onChange={props.runSearch}
+          ></input>
+    </div>
+  )
+}
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.addNumber}>
+      <div>
+        name: 
+        <input
+          value={props.newName}
+          onChange={props.handleValueChange}
+        />
+      </div>
+      <div>
+        number:
+        <input 
+          value={props.newNumber}
+          onChange={props.handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = (props) => {
+  return (
+  <>
+    {props.persons.map((person) =>
+      person.name.toLowerCase().includes(props.searchValue.toLowerCase())
+        ? (<li key={person.name}>{person.name} {person.number}</li>)
+        : null
+      )}
+  </>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '0401234567' },
-    { name: 'Arto Hella', number: '0401239967' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
+  const [searchValue, setNewSearchValue] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  
   const addNumber = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
@@ -31,35 +83,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const runSearch = (event) => {
+    console.log(event.target.value)
+    setNewSearchValue(event.target.value)
+
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNumber}>
-        <div>
-          name: 
-          <input
-            value={newName}
-            onChange={handleValueChange}
-          />
-        </div>
-        <div>
-          number:
-          <input 
-            value={newNumber}
-            onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-        <div>debug: {newName}, {newNumber}</div>
-      </form>
+      <Filter value={searchValue} runSearch={runSearch}/>
+      <h2>Add a new</h2>
+      <PersonForm newName={newName} newNumber={newNumber} handleValueChange={handleValueChange} handleNumberChange={handleNumberChange} addNumber={addNumber} />
       <h2>Numbers</h2>
-      <>
-        {persons.map((person) =>
-          <li key={person.name}>{person.name} {person.number}</li>
-        )}
-      </>
+      <Persons persons={persons} searchValue={searchValue} />
     </div>
   )
 
